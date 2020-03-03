b89853391529bc558e1f6249191bd2c7e3ba74ee
tvp100 <qyc100@gmail.com>
2020-03-04 00:13:55 +0800

    add flyway

--- src/main/java/com/tvp100/community/controller/IndexController.java
+++ src/main/java/com/tvp100/community/controller/IndexController.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class IndexController {
 
-    @Autowired
+    @Autowired(required = false)
     private UserMapper userMapper;
 
     @GetMapping("/")
