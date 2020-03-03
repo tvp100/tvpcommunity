d6a68987b92efd867b5d91d4556a8a598bb17686
tvp100 <qyc100@gmail.com>
2020-03-03 10:44:22 +0800

    extract github config form code to properties

--- src/main/java/com/tvp100/community/controller/AuthorizeController.java
+++ src/main/java/com/tvp100/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.tvp100.community.dto.AccessTokenDTO;
 import com.tvp100.community.dto.GithubUser;
 import com.tvp100.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("f1dc16ae477b1a50128f");
-        accessTokenDTO.setClient_secret("7b38f3fd7566e288be90f01ea19057fa1f7a6584");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
